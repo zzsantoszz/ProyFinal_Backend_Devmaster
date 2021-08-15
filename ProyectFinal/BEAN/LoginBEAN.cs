11dc68aa89cc962900ceaaf1d9a9711e7d8877e0
zzsantoszz <santsmax12@gmail.com>
2021-08-14 23:51:49 -0500

    EntityF_y_estructuraProy

---
+++ ProyectFinal/BEAN/LoginBEAN.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEAN
+{
+    class LoginBEAN
+    {
+        public int User { get; set; }
+        public int Pass { get; set; }
+    }
+}
---
+++ ProyectFinal/SistemaWEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SistemaWEB.MvcApplication" Language="C#" %>
