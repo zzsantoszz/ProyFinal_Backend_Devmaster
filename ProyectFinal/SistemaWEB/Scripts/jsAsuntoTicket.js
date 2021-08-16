904bf8ec9cdc616e8403d2678e08a53750c658fc
zzsantoszz <santsmax12@gmail.com>
2021-08-15 19:12:48 -0500

    CRUD categoria y asunto

---
+++ ProyectFinal/SistemaWEB/Scripts/jsAsuntoTicket.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    console.log("Registar Usuario");
+});
+
+function modalCrearAsuntoTicket() {
+    $("#modalPopup").removeClass("modal-lg");
+    $("#modalPopup").removeClass("modal-xs");
+    $("#modalPopup").removeClass("modal-md");
+    $("#modalPopup").removeClass("modal-sm");
+    $("#modalPopup").addClass("modal-md");
+
+    $.ajax({
+        type: 'GET',
+        url: '/AsuntoTicket/New',
+        beforeSend: function () {
+        },
+        success: function (data) {
+            $("#headerModal").html("Registrar Asunto: ");
+            $("#bodyModal").html(data);
+            //$('#btnGrabarModal').show();
+            $('#footerModal').show();
+            $('#myModal').modal();
+        },
+        error: function (data) {
+            console.log(data);
+        },
+        complete: function () {
+            //init_evt_ListaDetalleTesisParametro();
+        }
+    });
+}
---
+++ ProyectFinal/SistemaWEB/Scripts/jsCategoriaTicket.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    console.log("Registar Categoria");
+});
+
+function modalCrearCategoriaTicket() {
+    $("#modalPopup").removeClass("modal-lg");
+    $("#modalPopup").removeClass("modal-xs");
+    $("#modalPopup").removeClass("modal-md");
+    $("#modalPopup").removeClass("modal-sm");
+    $("#modalPopup").addClass("modal-md");
+
+    $.ajax({
+        type: 'GET',
+        url: '/CategoriaTicket/New',
+        beforeSend: function () {
+        },
+        success: function (data) {
+            $("#headerModal").html("Registrar Categoria: ");
+            $("#bodyModal").html(data);
+            //$('#btnGrabarModal').show();
+            $('#footerModal').show();
+            $('#myModal').modal();
+        },
+        error: function (data) {
+            console.log(data);
+        },
+        complete: function () {
+            //init_evt_ListaDetalleTesisParametro();
+        }
+    });
+}
